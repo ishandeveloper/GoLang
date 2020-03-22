405969e8b04f86ff843c281b7584f35541e9fbbe
Ishan Sharma <ishandeveloper@outlook.com>
2020-03-22 16:55:09 +0530

    User Inputs

---
+++ basics/user_inputs.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// imports fmt,bufio,os,strconv,strings
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter Your Name & Age : ")
+	name, _ := reader.ReadString('\n')
+
+	ageinput, _ := reader.ReadString('\n')
+	age, _ := strconv.ParseFloat(strings.TrimSpace(ageinput), 64)
+	fmt.Println(name)
+	fmt.Println(age + 2)
+
+}

3a2facf26b17fd2bf1a42dc094791bfc815f14d2
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-08 22:32:50 +0530

    Routes

--- 14_web/main.go
+++ 14_web/main.go
@@ -12,3 +12,9 @@ func index(w http.ResponseWriter, r *http.Request) {
 func about(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>About</h1>")
 }
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
+	fmt.Println("Server Starting...")
+	http.ListenAndServe(":3000", nil)
+}

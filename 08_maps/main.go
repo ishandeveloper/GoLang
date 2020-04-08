73312e2c40230a9dad085825835fb5c904375b5c
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-08 22:06:32 +0530

    Maps

--- 08_maps/main.go
+++ 08_maps/main.go
@@ -1,5 +1,11 @@
 package main
 
 func main() {
+	// Define map
+	emails := make(map[string]string)
 
+	//  Assign kv
+	emails["Bob"] = "bob@gmail.com"
+	emails["Sharon"] = "sharon@gmail.com"
+	emails["Mike"] = "mike@gmail.com"
 }

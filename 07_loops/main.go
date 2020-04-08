d895a08febeb724363abc702e9976b8772e93f74
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-08 22:05:32 +0530

    FizzBuzz

--- 07_loops/main.go
+++ 07_loops/main.go
@@ -15,4 +15,16 @@ func main() {
 		fmt.Printf("Number %d\n", i)
 	}
 
+	// FizzBuzz
+	for i := 1; i <= 100; i++ {
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
 }

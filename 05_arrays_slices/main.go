7233f00ac1999490774acd4f2e1e21d22c954dab
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-08 21:51:20 +0530

    Arrays

--- 05_arrays_slices/main.go
+++ 05_arrays_slices/main.go
@@ -4,11 +4,14 @@ import "fmt"
 
 func main() {
 	// Arrays
-	var fruitArr [2]string
+	// var fruitArr [2]string
 
-	// Assign values
-	fruitArr[0] = "Apple"
-	fruitArr[1] = "Orange"
+	// // Assign values
+	// fruitArr[0] = "Apple"
+	// fruitArr[1] = "Orange"
+
+	// Decalre and assign
+	fruitArr := [2]string{"Apple", "Orange"}
 
 	fmt.Println(fruitArr)
 }

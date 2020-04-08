adc1d6388f48793bbd98572a9e7e0210e0ca59d1
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-08 22:01:48 +0530

    Else If

--- 06_conditionals/main.go
+++ 06_conditionals/main.go
@@ -12,5 +12,15 @@ func main() {
 	} else {
 		fmt.Printf("%d is less than %d\n", y, x)
 	}
+	// else if
+	color := "red"
+
+	if color == "red" {
+		fmt.Println("color is red")
+	} else if color == "blue" {
+		fmt.Println("color is blue")
+	} else {
+		fmt.Println("color is NOT blue or red")
+	}
 
 }

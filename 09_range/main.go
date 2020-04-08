70de6ab3dda195e15aa4088c36aabccd21547c0f
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-08 22:17:27 +0530

    Range

--- 09_range/main.go
+++ 09_range/main.go
@@ -1,7 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	ids := []int{33, 76, 54, 23, 11, 2}
-
+	// Loop through ids
+	for i, id := range ids {
+		fmt.Printf("%d - ID: %d\n", i, id)
+	}
 }

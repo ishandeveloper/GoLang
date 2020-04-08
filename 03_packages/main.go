f6ad75a00db61533a352da7d39ab258615f40ee9
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-08 21:38:27 +0530

    Added Custom Package

--- 03_packages/main.go
+++ 03_packages/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"math"
+
+	"github.com/ishandeveloper/03_packages/strutil/"
 )
 
 func main() {
 	fmt.Println(math.Floor(2.7))
 	fmt.Println(math.Ceil(2.7))
 	fmt.Println(math.Sqrt(16))
-
+	fmt.Println(strutil.Reverse("hello"))
 }

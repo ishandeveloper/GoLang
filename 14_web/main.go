5e215ca2ce7c8428653c0d1cc5ef1911d28f42ed
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-08 22:32:37 +0530

    About Page

--- 14_web/main.go
+++ 14_web/main.go
@@ -8,3 +8,7 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello World</h1>")
 }
+
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>About</h1>")
+}

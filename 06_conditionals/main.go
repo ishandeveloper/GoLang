af0754b4fffec71899eb852b343b39a102827368
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-08 21:57:05 +0530

    Conditionals

--- 06_conditionals/main.go
+++ 06_conditionals/main.go
@@ -1,7 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 	x := 10
 	y := 10
+
+	//  If else
+	if x <= y {
+		fmt.Printf("%d is less than or equal to %d\n", x, y)
+	} else {
+		fmt.Printf("%d is less than %d\n", y, x)
+	}
 
 }

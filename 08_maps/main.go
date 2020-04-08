af1c08529ba939c5e3dd13346aee8ea6343cffa4
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-08 22:07:17 +0530

    Maps

--- 08_maps/main.go
+++ 08_maps/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	// Define map
 	emails := make(map[string]string)
@@ -13,4 +15,6 @@ func main() {
 	emails := map[string]string{"Bob": "bob@gmail.com", "Sharon": "sharon@gmail.com"}
 
 	emails["Mike"] = "mike@gmail.com"
+	fmt.Println(emails)
+	fmt.Println(len(emails))
 }
